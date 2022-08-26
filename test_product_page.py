cfc52c232cebc2b5fa28cd928bc0893c37815612
valeriazz <valeriazlk21@gmail.com>
2022-08-26 15:51:41 +0300

    some new tests

--- test_product_page.py
+++ test_product_page.py
@@ -3,26 +3,42 @@ from .pages.product_page import ProductPage
 from .pages.base_page import BasePage
 import time
 
-@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-                                  #"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-                                  #"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-                                  #"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-                                  #"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-                                  #"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-                                  #"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
-                                  #"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+#@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+#                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+#                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+#                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+#                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+#                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+#                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+#                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+#                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+#                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
  
-@pytest.mark.xfail
-def test_guest_can_add_books_to_basket(browser, link):
-    #link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-    page = ProductPage(browser, link)    
+#@pytest.mark.xfail
+#def test_guest_can_add_books_to_basket(browser):
+#    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+#    page = ProductPage(browser, link)    
+#    page.open()
+#    page.guest_cant_see_success_message()
+#    page.click_the_button_add_to_basket() 
+#    page.solve_quiz_and_get_code()
+#    page.guest_cant_see_success_message_after_adding_product_to_basket()
+#    page.message_disappeared_after_adding_product_to_basket()
+#    page.add_to_basket_alert_success()
+#    page.product_name_is_the_same()
+#    page.price_is_the_same()
+
+    
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
     page.open()
-    page.click_the_button_add_to_basket() 
-    page.solve_quiz_and_get_code()
-    time.sleep(5)
-    page.add_to_basket_alert_success()
-    page.product_name_is_the_same()
-    page.price_is_the_same()
-    time.sleep(5)
+    page.should_be_login_link()
+    
+    
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    
