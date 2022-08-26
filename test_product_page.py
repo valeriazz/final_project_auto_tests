fcc6d6f761000d383bac69d07a6ff42b185cc796
valeriazz <valeriazlk21@gmail.com>
2022-08-26 11:25:12 +0300

    add product page tests

---
+++ test_product_page.py
@@ -0,0 +1,28 @@
+import pytest
+from .pages.product_page import ProductPage
+from .pages.base_page import BasePage
+import time
+
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  #"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  #"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  #"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  #"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  #"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  #"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                                  #"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+ 
+@pytest.mark.xfail
+def test_guest_can_add_books_to_basket(browser, link):
+    #link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = ProductPage(browser, link)    
+    page.open()
+    page.click_the_button_add_to_basket() 
+    page.solve_quiz_and_get_code()
+    time.sleep(5)
+    page.add_to_basket_alert_success()
+    page.product_name_is_the_same()
+    page.price_is_the_same()
+    time.sleep(5)
