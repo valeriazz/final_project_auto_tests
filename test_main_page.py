12e478ea0520da5d363ff33964d24a39753676c1
valeriazz <valeriazlk21@gmail.com>
2022-08-26 17:44:16 +0300

    new tests for BasketPage

--- test_main_page.py
+++ test_main_page.py
@@ -1,12 +1,25 @@
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
+from .pages.base_page import BasePage
+from .pages.basket_page import BasketPage
+import time
+from selenium.common.exceptions import NoSuchElementException
 
-
-def test_guest_can_go_to_login_page(browser):
+#def test_guest_can_go_to_login_page(browser):
+    #link = "http://selenium1py.pythonanywhere.com/"
+    #page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    #page.open()                      # открываем страницу
+    #login_page = page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+    #login_page = LoginPage(browser, browser.current_url)
+    #login_page.should_be_login_page()
+   
+   
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
-    page.open()                      # открываем страницу
-    login_page = page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
-   
+    page2 = BasketPage(browser, link)
+    page.open()
+    page.go_to_basket()
+    time.sleep(10)
+    page2.text_about_empty_basket()
+    
